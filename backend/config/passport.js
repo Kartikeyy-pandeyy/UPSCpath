c9bd67e124c1fd2a1a5487e4f821e28046e4e78e
Kartikeyy-pandeyy <kartikeyy.pandeyy@gmail.com>
2025-04-01 15:30:02 +0530

    initial draft

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.PORT === "5000" ? "http://localhost:5000" : "https://upscpath-production.up.railway.app"}/auth/google/callback`,
+      callbackURL: "https://upscpath-production.up.railway.app/auth/google/callback", // Hardcode production URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
